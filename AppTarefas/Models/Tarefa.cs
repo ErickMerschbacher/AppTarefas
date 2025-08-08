e7247aff7f32a3ef0e0c5fe972f62f14ab018174
ErickMerschbacher <erick.m.silva21@aluno.senai.br>
2025-08-08 16:37:34 -0300

    Adiciona funcionalidade inicial de gerenciamento de tarefas
    - Alterado `_Layout.cshtml` para ajustar idioma, formatação e
      adicionar link para "Lista de Tarefas".
    - Criado `TarefasController` com ação `Index`.
    - Adicionado modelo `Tarefa` com propriedades básicas.
    - Criada view `Index.cshtml` para exibir lista de tarefas.

---
+++ AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        //Nome da chave primária deve ser da classe + "Id"
+        public int TarefasId { get; set; } //Id é a chave primária
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+
+    }
+}
